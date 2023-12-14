fddc2246c15e37692df8f9292cf645d298baa017
Darci (Jitera) <developers@jitera.com>
2023-12-14 14:23:41 +0000

    feat[business-logic]: implement 'Validate todo title uniqueness' Changes * '/app/services/todo_service/validate_details.rb': + Implement a method `validate_title_uniqueness` within the `TodoService::ValidateDetails` class that takes `user_id` and `title` as parameters..+ Query the `todos` table using the ActiveRecord query interface to check for the existence of a todo with the same `title` scoped to the `user_id`..+ If a...

--- app/services/todo_service/validate_details.rb
+++ app/services/todo_service/validate_details.rb
@@ -1,3 +1,4 @@
+# PATH: /app/services/todo_service/validate_details.rb
 # rubocop:disable Style/ClassAndModuleChildren
 class TodoService::ValidateDetails
   attr_accessor :details
@@ -5,19 +6,21 @@ class TodoService::ValidateDetails
     @details = details
   end
   def execute
-    return send_response('Invalid details') unless title_unique && due_date_valid
-    send_response('Details are valid')
+    uniqueness_result = validate_title_uniqueness(details[:user_id], details[:title])
+    return send_response('Title is already in use', false) unless uniqueness_result[:is_unique]
+    return send_response('Invalid due date', false) unless due_date_valid
+    send_response('Details are valid', true)
+  end
+  def validate_title_uniqueness(user_id, title)
+    is_unique = !Todo.exists?(user_id: user_id, title: title)
+    { is_unique: is_unique }
   end
   private
-  def title_unique
-    Todo.where(user_id: details[:user_id], title: details[:title]).empty?
-  end
   def due_date_valid
-    return false unless details[:due_date].future?
-    Todo.where(user_id: details[:user_id]).where('due_date = ?', details[:due_date]).empty?
+    details[:due_date].is_a?(Date) && details[:due_date].future?
   end
-  def send_response(message)
-    { message: message }
+  def send_response(message, is_unique)
+    { message: message, is_unique: is_unique }
   end
 end
 # rubocop:enable Style/ClassAndModuleChildren
