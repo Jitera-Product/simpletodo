f54381d838642d86a66ec2b045ef03d176db6657
Kylila (Jitera) <developers@jitera.com>
2023-12-14 14:32:24 +0000

    feat[business-logic]: implement 'Process and store attachments' Changes * '/app/services/todo_attachments_service.rb': + In the `TodoAttachmentsService`, define a private method `validate_attachment` that checks if the file format is allowed and if the file size does not exceed the maximum limit. Use the constants defined in the initializers for validation..+ Define a method `store_attachment` that takes a file and ...

--- app/services/todo_attachments_service.rb
+++ app/services/todo_attachments_service.rb
@@ -1,24 +1,45 @@
 # /app/services/todo_attachments_service.rb
 class TodoAttachmentsService
-  def save_attachments(todo_id, attachments)
-    attachments_info = []
-    attachments_saved = 0
+  MAX_FILE_SIZE = 10.megabytes # Example constant, replace with actual value from initializer
+  ALLOWED_FORMATS = %w[image/jpeg image/png application/pdf].freeze # Example constant, replace with actual value from initializer
+  def process_attachments(todo_id, attachments)
+    attachment_ids = []
+    errors = []
     attachments.each do |attachment|
-      begin
-        file_name = FileStorageService.generate_unique_name(attachment.original_filename)
-        file_path = FileStorageService.save_file(attachment, file_name)
-        Attachment.create!(
-          todo_id: todo_id,
-          file_path: file_path,
-          file_name: attachment.original_filename
-        )
-        attachments_info << { file_name: attachment.original_filename, file_path: file_path }
-        attachments_saved += 1
-      rescue => e
-        # Handle file upload errors here
-        Rails.logger.error "Failed to save attachment: #{e.message}"
+      if validate_attachment(attachment)
+        file_reference = store_attachment(attachment)
+        attachment_record = create_attachment_record(file_reference, todo_id)
+        attachment_ids << attachment_record.id
+      else
+        errors << "Invalid file format or size for #{attachment.original_filename}"
       end
     end
-    { attachments_saved: attachments_saved, attachments_info: attachments_info }
+    if errors.any?
+      { success: false, errors: errors }
+    else
+      { success: true, attachment_ids: attachment_ids }
+    end
+  end
+  private
+  def validate_attachment(attachment)
+    file_size = attachment.size
+    file_format = attachment.content_type
+    file_size <= MAX_FILE_SIZE && ALLOWED_FORMATS.include?(file_format)
+  end
+  def store_attachment(attachment)
+    # Assuming Active Storage is set up
+    blob = ActiveStorage::Blob.create_and_upload!(
+      io: attachment,
+      filename: attachment.original_filename,
+      content_type: attachment.content_type
+    )
+    blob.signed_id # or blob.key, depending on your needs
+  end
+  def create_attachment_record(file_reference, todo_id)
+    Attachment.create!(
+      todo_id: todo_id,
+      file: file_reference,
+      file_name: File.basename(file_reference)
+    )
   end
 end
