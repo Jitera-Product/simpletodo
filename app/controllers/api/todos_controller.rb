b56f5f5ffdb0310f9e6b58b52c2e81e3e78d26c8
Ardra (Jitera) <developers@jitera.com>
2023-12-14 14:36:47 +0000

    feat[api]: implement 'Associate Todo with Categories' Changes * '/app/controllers/api/todos_controller.rb': + Add a new action `associate_categories` within the `Api::TodosController` to handle the POST request to `/api/todos/{todo_id}/categories`..+ Use the `before_action` to authenticate and authorize the user, ensuring they have the necessary permissions to associate categories with a todo item..+ Find...

--- app/controllers/api/todos_controller.rb
+++ app/controllers/api/todos_controller.rb
@@ -1,5 +1,6 @@
 class Api::TodosController < Api::BaseController
-  before_action :doorkeeper_authorize!, only: %i[create destroy cancel_deletion validate_due_date]
+  before_action :doorkeeper_authorize!, only: %i[create destroy cancel_deletion associate_categories validate_due_date]
+  before_action :set_todo, only: %i[associate_categories]
   def create
     @todo = TodoService::Create.new(create_params, current_resource_owner).execute
     if @todo
@@ -25,6 +26,19 @@ class Api::TodosController < Api::BaseController
       render json: { status: 200, message: result[:message] }, status: :ok
     end
   end
+  def associate_categories
+    category_ids = params[:category_ids]
+    if category_ids.blank? || !Category.where(id: category_ids).exists?
+      render json: { error: 'One or more categories not found' }, status: :bad_request
+      return
+    end
+    begin
+      TodoCategoryAssociator.new(@todo, category_ids).execute
+      render json: { status: 200, todo: { id: @todo.id, categories: @todo.categories } }, status: :ok
+    rescue => e
+      render json: { error: e.message }, status: :internal_server_error
+    end
+  end
   def validate_due_date
     begin
       due_date = DateTime.parse(params[:due_date])
@@ -38,6 +52,10 @@ class Api::TodosController < Api::BaseController
     end
   end
   private
+  def set_todo
+    @todo = Todo.find_by(id: params[:todo_id])
+    render json: { error: 'Todo not found' }, status: :not_found unless @todo
+  end
   def create_params
     params.require(:todo).permit(:title, :description, :due_date, :category, :priority, :recurring, :attachment)
   end
