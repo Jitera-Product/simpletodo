9817e75d2d7c280d8de77923bb948170c0ffbb39
Berrie (Jitera) <developers@jitera.com>
2023-12-14 14:31:13 +0000

    feat[business-logic]: implement 'Validate due date' Changes * '/app/services/todo_service/validate_details.rb': + Implement a method `validate_due_date` within the `TodoService::ValidateDetails` class that takes a "due_date" parameter of type datetime..+ The method should first check if the "due_date" is a valid datetime object. You can use Ruby's built-in datetime parsing methods to ensure the format is corr...

--- app/services/todo_service/validate_details.rb
+++ app/services/todo_service/validate_details.rb
@@ -1,22 +1,37 @@
 # PATH: /app/services/todo_service/validate_details.rb
 # rubocop:disable Style/ClassAndModuleChildren
+require 'date'
 class TodoService::ValidateDetails
   attr_accessor :details
   def initialize(details)
     @details = details
   end
   def execute
-    uniqueness_result = validate_title_uniqueness(details[:title], details[:user_id])
-    raise StandardError, 'Title is already in use' unless uniqueness_result[:is_unique]
+    uniqueness_result = validate_title_uniqueness(details[:user_id], details[:title])
+    return send_response('Title is already in use', false) unless uniqueness_result[:is_unique]
+    due_date_result = validate_due_date(details[:due_date])
+    return due_date_result unless due_date_result[:is_valid]
     send_response('Details are valid', true)
   end
   private
-  def validate_title_uniqueness(title, user_id)
+  def validate_title_uniqueness(user_id, title)
     is_unique = !Todo.exists?(user_id: user_id, title: title)
     { is_unique: is_unique }
   end
-  def send_response(message, is_unique)
-    { message: message, is_unique: is_unique }
+  def validate_due_date(due_date)
+    begin
+      parsed_due_date = DateTime.parse(due_date)
+      if parsed_due_date > DateTime.now
+        { is_valid: true }
+      else
+        send_response('Due date must be set in the future', false)
+      end
+    rescue ArgumentError
+      send_response('Invalid due date format', false)
+    end
+  end
+  def send_response(message, is_valid)
+    { message: message, is_valid: is_valid }
   end
 end
 # rubocop:enable Style/ClassAndModuleChildren
