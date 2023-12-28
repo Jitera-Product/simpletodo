f353edc5f8c3a40d9caf9f33a15a35f7968d446a
Mathilde (Jitera) <developers@jitera.com>
2023-12-28 05:47:36 +0000

    feat[api]: implement 'Add Todo Item to Folder' Changes * '/app/controllers/api/todos_controller.rb': + To implement the "Add Todo Item to Folder" API, you will need to modify the `todos_controller.rb` file within the `Api` namespace..+ The `create` action should be updated to handle the new requirements for the todo item creation. You will need to use the...

--- app/controllers/api/todos_controller.rb
+++ app/controllers/api/todos_controller.rb
@@ -1,7 +1,7 @@
 class Api::TodosController < Api::BaseController
   before_action :doorkeeper_authorize!, only: %i[create destroy cancel_deletion]
-  before_action :authenticate_user!, only: [:index, :destroy] # Added :destroy to ensure user is authenticated
-  before_action :set_folder, only: [:index]
+  before_action :authenticate_user!, only: [:index, :create] # Added :create to ensure user is authenticated
+  before_action :set_folder, only: [:index, :create] # Updated to include :create action
 
   def index
     todos = @folder.todos.select(:id, :title, :description, :due_date, :priority, :status)
@@ -9,25 +9,27 @@ class Api::TodosController < Api::BaseController
   end
 
   def create
-    @todo = TodoService::Create.new(create_params, current_resource_owner).execute
-    if @todo
-      render :show, status: :created
+    # Validate create_params before proceeding
+    validator = TodoValidator.new(create_params)
+    unless validator.valid?
+      render json: { errors: validator.errors }, status: :unprocessable_entity and return
+    end
+
+    @todo = TodoService::Create.new(create_params.merge(user_id: current_resource_owner.id), current_resource_owner).execute
+    if @todo.persisted?
+      render json: { status: 201, todo: @todo.as_json }, status: :created, location: api_todo_url(@todo)
     else
-      render json: { error: 'Failed to create todo' }, status: :unprocessable_entity
+      render json: { errors: @todo.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   def destroy
-    todo_id = params[:id].to_i
-    return render json: { error: 'Invalid ID format.' }, status: :bad_request unless todo_id.is_a?(Integer) && todo_id > 0
-
-    @todo = Todo.find_by(id: todo_id, user_id: current_resource_owner.id) # Ensuring that the todo belongs to the current user
-    return render json: { error: 'Todo item not found.' }, status: :not_found unless @todo
-
-    if @todo.destroy # Using ActiveRecord's destroy method to delete the todo
-      render json: { status: 200, message: 'Todo item successfully deleted.' }, status: :ok
+    @todo = TodoService::ValidateTodo.new(params[:id], current_resource_owner.id).execute
+    if @todo
+      message = TodoService::Delete.new(params[:id], current_resource_owner.id).execute
+      render json: { status: 200, message: message }, status: :ok
     else
-      render json: { error: 'Failed to delete todo item.' }, status: :internal_server_error
+      render json: { error: 'This to-do item is not found' }, status: :unprocessable_entity
     end
   end
 
@@ -49,13 +51,47 @@ class Api::TodosController < Api::BaseController
   end
 
   def set_folder
-    @folder = Folder.find_by(id: params[:folder_id], user_id: @current_user.id)
+    @folder = Folder.find_by(id: params[:folder_id], user_id: current_resource_owner.id)
     unless @folder
       render json: { error: 'Folder not found or not owned by user' }, status: :not_found
     end
   end
 
   def create_params
-    params.require(:todo).permit(:title, :description, :due_date, :category, :priority, :recurring, :attachment)
+    # Merged the new and existing create_params
+    params.require(:todo).permit(:title, :description, :due_date, :category, :priority, :recurring, :attachment, :status, :folder_id)
+  end
+end
+
+# Validator class to handle the validation logic
+class TodoValidator
+  include ActiveModel::Validations
+
+  validates :title, presence: { message: 'The title is required.' }, length: { maximum: 200, too_long: 'The title cannot exceed 200 characters.' }
+  validates :description, length: { maximum: 1000, too_long: 'The description cannot exceed 1000 characters.' }
+  validates :due_date, presence: true, date: { message: 'Invalid due date format.' }
+  validates :priority, numericality: { only_integer: true, message: 'Invalid priority format.' }
+  validates :status, numericality: { only_integer: true, message: 'Invalid status format.' }
+  validate :folder_exists
+  validate :user_exists
+
+  attr_accessor :title, :description, :due_date, :priority, :status, :folder_id, :user_id
+
+  def initialize(params = {})
+    @title = params[:title]
+    @description = params[:description]
+    @due_date = params[:due_date]
+    @priority = params[:priority]
+    @status = params[:status]
+    @folder_id = params[:folder_id]
+    @user_id = params[:user_id]
+  end
+
+  def folder_exists
+    errors.add(:folder_id, 'Folder not found.') unless Folder.exists?(id: folder_id)
+  end
+
+  def user_exists
+    errors.add(:user_id, 'User not found.') unless User.exists?(id: user_id)
   end
 end
