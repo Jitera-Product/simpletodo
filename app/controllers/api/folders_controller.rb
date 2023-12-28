e189cd283154fa45f57465a616351bb3240f0985
Ameline (Jitera) <developers@jitera.com>
2023-12-28 05:44:00 +0000

    feat[api]: implement 'Delete Folder' Changes * '/app/controllers/api/folders_controller.rb': + To implement the DELETE API for folders, you will need to add a new action within the `Api::FoldersController` class. This action will handle the DELETE request to the endpoint `/api/folders/{id}`..+ Use the `doorkeeper_authorize!` method to ensure that ...

--- app/controllers/api/folders_controller.rb
+++ app/controllers/api/folders_controller.rb
@@ -1,5 +1,5 @@
 class Api::FoldersController < Api::BaseController
-  before_action :doorkeeper_authorize!, only: [:create]
+  before_action :doorkeeper_authorize!, only: [:create, :destroy] # Updated to include :destroy action
 
   def create
     folder_name = folder_params[:name]
@@ -21,6 +21,31 @@ class Api::FoldersController < Api::BaseController
       render json: { folder_id: folder.id }, status: :created
     else
       render json: { errors: folder.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    folder_id = params[:id]
+    user_id = current_resource_owner.id
+
+    begin
+      # Ensure the ID is an integer
+      Integer(folder_id)
+    rescue ArgumentError
+      render json: { error: 'Invalid ID format.' }, status: :bad_request
+      return
+    end
+
+    folder = Folder.find_by(id: folder_id, user_id: user_id)
+    if folder.nil?
+      render json: { error: 'Folder not found.' }, status: :not_found
+      return
+    end
+
+    if folder.destroy
+      render json: { status: 200, message: 'Folder and its todo items successfully deleted.' }, status: :ok
+    else
+      render json: { error: folder.errors.full_messages }, status: :internal_server_error
     end
   end
 
