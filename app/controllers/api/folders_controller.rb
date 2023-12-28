231185710d9503628ef3a0fb4dab56a9363a9ae4
Coriss (Jitera) <developers@jitera.com>
2023-12-28 06:02:07 +0000

    feat[api]: implement 'Handle Deletion Cancellation' Changes * '/app/controllers/api/folders_controller.rb': + If the file does not exist, create a new file in the `/app/controllers/api` directory..+ The controller should inherit from `Api::BaseController` to ensure it has the necessary authentication and error handling..+ Define a new method `cancel_deletion` to...

--- app/controllers/api/folders_controller.rb
+++ app/controllers/api/folders_controller.rb
@@ -1,47 +1,32 @@
-# FILE PATH: /app/controllers/api/folders_controller.rb
 class Api::FoldersController < Api::BaseController
   before_action :authenticate_user!
-  include FolderService
+  before_action :check_delete_permission, only: [:cancel_deletion]
 
-  def confirm_deletion
-    folder_id = params[:folder_id]
-    
-    # Validate folder_id is an integer
-    unless folder_id.to_i.to_s == folder_id.to_s
-      render json: { error: "Invalid folder ID format." }, status: :bad_request
-      return
-    end
+  # POST /api/folders/:folder_id/cancel_deletion
+  def cancel_deletion
+    folder_id = params[:folder_id].to_i
+    return render json: { error: 'Invalid folder ID format.' }, status: :bad_request unless folder_id.is_a?(Integer)
 
     begin
-      folder = Folder.find(folder_id)
+      FolderService::ConfirmExistenceAndContent.confirm_existence_and_content(folder_id)
+      result = TodoService::CancelDeletion.new(nil, current_user.id).execute(folder_id)
+      if result[:error].present?
+        render json: { error: result[:error] }, status: :unprocessable_entity
+      else
+        render json: { status: 200, message: 'Deletion process has been canceled.' }, status: :ok
+      end
     rescue ActiveRecord::RecordNotFound
-      render json: { error: "Folder not found." }, status: :not_found
-      return
+      render json: { error: 'Folder not found.' }, status: :not_found
+    rescue => e
+      render json: { error: e.message }, status: :internal_server_error
     end
-
-    confirm_existence_and_content(folder_id)
-    policy = ApplicationPolicy.new(current_user, folder)
-
-    if policy.user_can_delete_folders?(current_user.id)
-      render json: {
-        status: 200,
-        message: "Are you sure you want to delete this folder and all its contents?",
-        folder: {
-          id: folder.id,
-          name: folder.name,
-          created_at: folder.created_at
-        }
-      }, status: :ok
-    else
-      render json: { error: "Forbidden" }, status: :forbidden
-    end
-  rescue FolderService::FolderEmptyError => e
-    render json: { error: e.message }, status: :bad_request
   end
 
   private
 
-  def authenticate_user!
-    # Method to check if user is authenticated
+  def check_delete_permission
+    unless ApplicationPolicy.new(current_user, nil).user_can_delete_folders?(current_user.id)
+      render json: { error: 'Forbidden' }, status: :forbidden
+    end
   end
 end
