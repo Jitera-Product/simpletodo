afdded69561fddfba3636c9d908197fbe523bc51
Cacilia (Jitera) <developers@jitera.com>
2023-12-28 04:12:26 +0000

    feat[api]: implement 'Create Custom Folder' Changes * '/app/controllers/api/folders_controller.rb': + To implement the "Create Custom Folder" API, you will need to add a new action to the `Api::FoldersController` class. This action will handle the POST request to the `/api/folders` endpoint..+ The controller should inherit from `Api::BaseController` to e...

--- app/controllers/api/folders_controller.rb
+++ app/controllers/api/folders_controller.rb
@@ -1,18 +1,23 @@
 class Api::FoldersController < Api::BaseController
+  before_action :authenticate_user!
   before_action :load_user, only: [:create]
 
   def create
     folder_params = params.require(:folder).permit(:name, :user_id)
     begin
+      raise ArgumentError, "The folder name is required." if folder_params[:name].blank?
+      
       if folder_exists?(folder_params[:name], folder_params[:user_id])
-        render json: { status: 409, message: "Folder with the given name already exists." }, status: :conflict
+        render json: { status: 409, message: "Folder name already exists. Please choose a different name." }, status: :conflict
       else
-        folder = Folder.new(folder_params)
+        folder = @user.folders.build(folder_params)
         folder.save!
-        render json: { id: folder.id, name: folder.name, created_at: folder.created_at, updated_at: folder.updated_at }, status: :created
+        render json: { status: 201, folder: folder_response(folder) }, status: :created
       end
     rescue ActiveRecord::RecordNotFound
       render json: { status: 404, message: "User not found." }, status: :not_found
+    rescue ArgumentError => e
+      render json: { status: 400, message: e.message }, status: :bad_request
     rescue ActiveRecord::RecordInvalid => e
       render json: { status: 422, message: e.record.errors.full_messages }, status: :unprocessable_entity
     rescue StandardError => e
@@ -22,11 +27,32 @@ class Api::FoldersController < Api::BaseController
 
   private
 
+  def authenticate_user!
+    # Assuming there's a method to authenticate user
+    # This is a placeholder for actual authentication logic
+    raise "Unauthorized" unless user_signed_in?
+  end
+
+  def user_signed_in?
+    # Placeholder for checking if user is signed in
+    # This should be replaced with actual sign-in checking logic
+    true
+  end
+
   def load_user
     @user = User.find(params[:folder][:user_id])
   end
 
   def folder_exists?(name, user_id)
     Folder.exists?(name: name, user_id: user_id)
+  end
+
+  def folder_response(folder)
+    {
+      id: folder.id,
+      name: folder.name,
+      created_at: folder.created_at,
+      user_id: folder.user_id
+    }
   end
 end
