51404001e50185fe1423f0400a2e7ae9e98501af
June (Jitera) <developers@jitera.com>
2024-01-16 01:58:04 +0000

    fix: 1 issues from deployment report

--- app/models/todo_folder.rb
+++ app/models/todo_folder.rb
@@ -1,6 +1,13 @@
 
 class TodoFolder < ApplicationRecord
-  belongs_to :user
-
+  # validations
   validates :name, presence: true, uniqueness: { scope: :user_id }
+  # end for validations
+
+  # relationships
+  belongs_to :user
+  # end for relationships
+
+  class << self
+  end
 end
